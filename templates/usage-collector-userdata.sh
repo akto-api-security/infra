d934eeeb84775f99ee6e2cec541c1415a62fa9ec
aktoboy <bhavik@akto.io>
2023-08-02 18:09:23 +0530

    Fixed script

--- templates/usage-collector-userdata.sh
+++ templates/usage-collector-userdata.sh
@@ -7,9 +7,7 @@ export COMPOSE_FILE=docker-compose-usage.yml
 curl -fsSL 'https://raw.githubusercontent.com/akto-api-security/infra/feature/usage-infra/cf-deploy-akto' > cf-deploy-akto
 sudo chmod 700 cf-deploy-akto
 "./cf-deploy-akto < <(echo 'test')"
-sudo echo DB_CONN_RUL="{url}" >>
-                  ~/akto/infra/docker-usage.env
-
+sudo echo DB_CONN_RUL="{url}" >> ~/akto/infra/docker-usage.env
 curl -fsSL 'https://raw.githubusercontent.com/akto-api-security/infra/feature/usage-infra/cf-deploy-akto-start' > cf-deploy-akto-start
 sudo chmod 700 cf-deploy-akto-start
 "./cf-deploy-akto-start < <(echo 'test')"
