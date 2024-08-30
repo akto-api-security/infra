e617dd7d2a6dd007f4460086c07715a3deed6a2b
SHIVAM RAWAT <shivam@akto.io>
2024-08-30 05:59:49 +0530

    docDB setup using pulumi

--- pulumi-on-prem-akto-setup-with-daemonset/index.ts
+++ pulumi-on-prem-akto-setup-with-daemonset/index.ts
@@ -1,17 +1,77 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as helm from "@pulumi/kubernetes/helm/v3";
+import * as k8s from "@pulumi/kubernetes";
 import * as yaml from "@pulumi/kubernetes/yaml";
+import * as aws from "@pulumi/aws";
+
 
 const config = new pulumi.Config();
 const k8sNamespace = config.get("k8sNamespace") || "default";
 const aktoMongoConn = config.get("akto-mongo-connection") || "";
+const eksClusterName = config.get("eks-cluster-name") || "";
+
+const eksCluster = aws.eks.getCluster({
+    name: eksClusterName,
+});
+
+const vpcId = eksCluster.then(cluster => cluster.vpcConfig.vpcId);
+
+const subnets = eksCluster.then(cluster => aws.ec2.getSubnets({
+    filters: [
+        { name: "vpc-id", values: [cluster.vpcConfig.vpcId] },
+    ],
+}));
+
+const docdbSecurityGroup = new aws.ec2.SecurityGroup("docdbSecurityGroup", {
+    vpcId: vpcId,
+    ingress: [
+        {
+            protocol: "tcp",
+            fromPort: 27017,
+            toPort: 27017,
+            cidrBlocks: ["0.0.0.0/0"],  // Adjust to restrict access to specific IP ranges
+        },
+    ],
+    egress: [
+        {
+            protocol: "-1",
+            fromPort: 0,
+            toPort: 0,
+            cidrBlocks: ["0.0.0.0/0"],
+        },
+    ],
+});
+
+const docdbSubnetGroup = new aws.docdb.SubnetGroup("docdbsubnetgroup", {
+    subnetIds: subnets.then(subnets => subnets.ids),
+});
+
+const docdbCluster = new aws.docdb.Cluster("aktoDocDBCluster", {
+    clusterIdentifier: "akto-docdb-cluster",
+    engine: "docdb",
+    masterUsername: "angellist",
+    masterPassword: "eightdigitpassword",
+    dbSubnetGroupName: docdbSubnetGroup.name,
+    vpcSecurityGroupIds: [docdbSecurityGroup.id],
+    applyImmediately: true
+});
+
+const docdbInstance = new aws.docdb.ClusterInstance("exampleInstance", {
+    identifier: "example-docdb-instance",
+    clusterIdentifier: docdbCluster.id,
+    instanceClass: "db.r5.large",  // Adjust the instance class as needed
+    applyImmediately: true,
+});
+
+const connectionString = pulumi.interpolate`mongodb://${docdbCluster.masterUsername}:${docdbCluster.masterPassword}@${docdbCluster.endpoint}:${docdbCluster.port}/?ssl=false&retryWrites=false`;
+
 const miniRuntimeChart = new helm.Chart("akto", {
     version: "0.1.8",
     path: "./akto-setup",
     namespace: k8sNamespace,
     values: {
         mongo: {
-            aktoMongoConn: aktoMongoConn
+            aktoMongoConn: connectionString
         }
     },
 });
@@ -32,7 +92,7 @@ const daemonsetEbpf = new yaml.ConfigFile("daemonset-ebpf", {
                             container.env[env].value = "akto-runtime." + k8sNamespace + ".svc.cluster.local:9092"
                         }
                         if (container.env[env].name === "AKTO_MONGO_CONN") {
-                            container.env[env].value = aktoMongoConn
+                            container.env[env].value = connectionString
                         }
                     }
                 });
