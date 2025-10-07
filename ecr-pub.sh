4ba5d208024c76d2d7e30546e87d01152238e627
orenakto <oren+1@akto.io>
2025-10-07 11:58:39 +0530

    update mirror script and repo lifecycle policy

--- ecr-pub.sh
+++ ecr-pub.sh
@@ -1,7 +1,9 @@
-AWS_REGION="ap-south-1"
+ECR_PUBLIC_REGION="us-east-1"
+
+ECR_PRIVATE_REGION="ap-south-1"
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-ECR_PRIVATE_REGISTRY="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+ECR_PRIVATE_REGISTRY="$ACCOUNT_ID.dkr.ecr.$ECR_PRIVATE_REGION.amazonaws.com"
 ECR_PRIVATE_REPO_NAMESPACE="ecr-scan-mirror"
 
 get_latest_image_info() {
@@ -9,7 +11,7 @@ get_latest_image_info() {
 
     local repository_name="$1"
 
-    local latest_image_info=$(aws ecr describe-images --repository-name "$repository_name" --region "$AWS_REGION" \
+    local latest_image_info=$(aws ecr-public describe-images --repository-name "$repository_name" --region "$ECR_PUBLIC_REGION" \
         --query 'sort_by(imageDetails, &imagePushedAt)[-1]' \
         --output json 2>/dev/null)
 
@@ -22,34 +24,38 @@ get_all_public_ecr_repos() {
 	local next_token=""
 	local all_repos="[]"
 	while : ; do
+        echo -e "\nFetching ecr public repositories with next token: '$next_token'" >&2
+
 		if [ -z "$next_token" ]; then
-			response=$(aws ecr describe-repositories --region "$AWS_REGION" --output json)
+			response=$(aws ecr-public describe-repositories --region "$ECR_PUBLIC_REGION" --output json)
 		else
-			response=$(aws ecr describe-repositories --region "$AWS_REGION" --output json --starting-token "$next_token")
+			response=$(aws ecr-public describe-repositories --region "$ECR_PUBLIC_REGION" --output json --starting-token "$next_token")
 		fi
+
+        echo "Response: $response" >&2
         
 		repos=$(echo "$response" | jq -c '.repositories[] | {name: .repositoryName, uri: .repositoryUri}')
 		updated_repos="[]"
-		while IFS= read -r repo; do
-			# Add is_public: true (or set based on a condition)
-            repository_name=$(echo "$repo" | jq -r '.name')
-            if [[ "$repository_name" == "$ECR_PRIVATE_REPO_NAMESPACE"* ]]; then
-                continue
-            fi
 
-            latest_image_info=$(get_latest_image_info "$repository_name")
-            # Only add repo if latest_image_info is valid JSON, not null, not empty, and has at least one tag or digest
-            if jq -e . >/dev/null 2>&1 <<<"$latest_image_info" \
-                && [ "$latest_image_info" != "null" ] \
-                && [ "$latest_image_info" != "{}" ] \
-                && [ -n "$latest_image_info" ] \
-                && { [ "$(echo "$latest_image_info" | jq -r '.imageTags | length')" -gt 0 ] || [ "$(echo "$latest_image_info" | jq -r '.imageDigest')" != "null" ]; }; then
-                repo=$(echo "$repo" | jq --argjson img_info "$latest_image_info" '. + {latest_image_info: $img_info}')
-                updated_repos=$(jq -s 'add' <(echo "$updated_repos") <(echo "[$repo]"))
-            fi
-			
-		done <<< "$repos"
-		all_repos=$(jq -s 'add' <(echo "$all_repos") <(echo "$updated_repos"))
+        if [ -n "$repos" ]; then
+            while IFS= read -r repo; do
+                [ -z "$repo" ] && continue
+                repository_name=$(echo "$repo" | jq -r '.name')
+
+                latest_image_info=$(get_latest_image_info "$repository_name")
+                # Only add repo if latest_image_info is valid JSON, not null, not empty, and has at least one tag or digest
+                if jq -e . >/dev/null 2>&1 <<<"$latest_image_info" \
+                    && [ "$latest_image_info" != "null" ] \
+                    && [ "$latest_image_info" != "{}" ] \
+                    && [ -n "$latest_image_info" ] \
+                    && { [ "$(echo "$latest_image_info" | jq -r '.imageTags | length')" -gt 0 ] || [ "$(echo "$latest_image_info" | jq -r '.imageDigest')" != "null" ]; }; then
+                    repo=$(echo "$repo" | jq --argjson img_info "$latest_image_info" '. + {latest_image_info: $img_info}')
+                    updated_repos=$(jq -s 'add' <(echo "$updated_repos") <(echo "[$repo]"))
+                fi
+                
+            done <<< "$repos"
+            all_repos=$(jq -s 'add' <(echo "$all_repos") <(echo "$updated_repos"))
+        fi
 		
 		next_token=$(echo "$response" | jq -r '.nextToken // empty')
 		[ -z "$next_token" ] && break
@@ -59,13 +65,13 @@ get_all_public_ecr_repos() {
 
 check_private_repo_exists() {
     local repository_name="$1"
-    aws ecr describe-repositories --repository-names "$ECR_PRIVATE_REPO_NAMESPACE/$repository_name" --region $AWS_REGION > /dev/null 2>&1
+    aws ecr describe-repositories --repository-names "$ECR_PRIVATE_REPO_NAMESPACE/$repository_name" --region $ECR_PRIVATE_REGION > /dev/null 2>&1
     if [ $? -ne 0 ]; then
       echo "ECR Private Repo $ECR_PRIVATE_REPO_NAMESPACE/$repository_name does not exist. Creating..." 
-      aws ecr create-repository --repository-name "$ECR_PRIVATE_REPO_NAMESPACE/$repository_name" --region $AWS_REGION > /dev/null
+      aws ecr create-repository --repository-name "$ECR_PRIVATE_REPO_NAMESPACE/$repository_name" --region $ECR_PRIVATE_REGION > /dev/null
 
       # Apply lifecycle policy to the private repo - delete untagged images (create after image push)
-      aws ecr put-lifecycle-policy --repository-name "$ECR_PRIVATE_REPO_NAMESPACE/$repository_name" --lifecycle-policy-text "$(cat ecr-scan-mirror-lifecycle-policy.json)" --region $AWS_REGION > /dev/null
+      aws ecr put-lifecycle-policy --repository-name "$ECR_PRIVATE_REPO_NAMESPACE/$repository_name" --lifecycle-policy-text "$(cat ecr-scan-mirror-lifecycle-policy.json)" --region $ECR_PRIVATE_REGION > /dev/null
       echo "ECR Private Repo created."
     else
       echo "ECR Private Repo $ECR_PRIVATE_REPO_NAMESPACE/$repository_name already exists."
